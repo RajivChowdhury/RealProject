a18873ded7b1bdc7604087a2d749469592b5cb10
RajivChowdhury <onsitesimplyrajiv@gmail.com>
2017-09-20 22:56:37 -0700

    X

--- src/com/git/test/Vehicle.java
+++ src/com/git/test/Vehicle.java
@@ -6,4 +6,12 @@ public class Vehicle {
 	private String vin;
 	private String engine;
 
+	public String getSteering() {
+		return steering;
+	}
+
+	public void setSteering(String steering) {
+		this.steering = steering;
+	}
+
 }
